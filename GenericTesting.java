ebf36d7c32987d759f3eaa324b3e923c8474b3a4
pbarot10 <pbarot10@vt.edu>
2019-11-06 19:03:35 +0100

    updated GenericTesting

--- GenericTesting.java
+++ GenericTesting.java
@@ -1,3 +1,19 @@
 public class GenericTesting{
     
+    function largestElement(arr) {
+        var results = [];
+        for (var n = 0; n < arr.length; n++) {
+            var largestNumber = arr[n][0];
+            for (var sb = 1; sb < arr[n].length; sb++) {
+                if (arr[n][sb] > largestNumber) {
+                    largestNumber = arr[n][sb];
+                }
+            }
+
+            results[n] = largestNumber;
+        }
+
+        return results;
+    }
+
 }
