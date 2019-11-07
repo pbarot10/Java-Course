689de8e87b92bffaa5727ada383a7112fa120101
pbarot10 <pbarot10@vt.edu>
2019-11-07 18:22:02 +0100

    In-class activity person.java updated

--- Person.java
+++ Person.java
@@ -1,4 +1,15 @@
-public class Person{
-    public String name;
+import java.utiil.Scanner;
 
+public class Person{
+     String name, lastName, city;
+     int age;
+     boolean sex;
+
+    public Person(String name, String lastName, int age, boolean sex, String city) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+        this.sex = sex;
+        this.city = city;
+    }
 }
