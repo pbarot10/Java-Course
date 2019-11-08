4470cd9b5194cb40210292e04497602f590a826c
pbarot10 <pbarot10@vt.edu>
2019-11-08 18:09:36 +0100

    wrote test

--- src/test/java/MathTest.java
+++ src/test/java/MathTest.java
@@ -1,9 +1,12 @@
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertEquals;
+
 public class MathTest {
 
     @Test
     public void testSum() {
+        assertEquals(10, Math.addExact(7, 3));
 
     }
 }
