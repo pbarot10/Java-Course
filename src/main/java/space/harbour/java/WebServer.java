a04c595d671b0206416c6af197391d1562443eb7
pbarot10 <pbarot10@vt.edu>
2019-11-20 17:40:44 +0100

    added the webServer activity

---
+++ src/main/java/space/harbour/java/WebServer.java
@@ -0,0 +1,8 @@
+package space.harbour.java;
+import static spark.Spark.*;
+
+public class WebServer {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
+    }
+}
